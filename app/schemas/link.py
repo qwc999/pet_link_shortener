91e82f2043e510be2092f7e1e68ee6d4100bb959
Alex <azhidenko13@gmail.com>
2025-11-17 23:09:37 +0300

    link url validation

--- app/schemas/link.py
+++ app/schemas/link.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, field_validator, HttpUrl
+import validators
 
 
 class LinkResponse(BaseModel):
@@ -17,3 +18,14 @@ class LinkDelete(BaseModel):
 
 class LinkCreate(BaseModel):
     original_url: HttpUrl
+    # mode=before позволяет выполнить валидацию и только потом переделать в объект HttpUrl
+    @field_validator("original_url", mode="before")
+    @classmethod
+    def pre_validate_url(cls, v: str) -> str:
+        if isinstance(v, str):
+            v = v.strip()
+            if not v.startswith(("https://", "http://")):
+                v = "https://" + v
+            if not validators.url(v):
+                raise ValueError("Invalid URL format")
+        return v
